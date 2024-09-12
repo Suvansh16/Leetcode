ec585a80536409b8d0811e5c440da3f4bfd32ceb
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-09-13 00:21:15 +0530

    Time: 12 ms (55.24%), Space: 20.4 MB (12.28%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
-int temp[100001];
-int solve(int start,vector<int>&nums)
-{
-    if(start==nums.size()-1)
-    return 0;
-    if(start>=nums.size())
-    return INT_MAX;
-    if(temp[start]!=-1)
-    return temp[start];
-    int steps=INT_MAX;
-    int minsteps=min(nums[start]+start,(int)nums.size()-1);
-    for(int i=start+1;i<=minsteps;i++)
-    {
-        int a=solve(i,nums);
-        if(a!=INT_MAX)
-        {
-            steps=min(steps,1+a);
-        }
-    }
-    temp[start]=steps;
-    return temp[start];
-
-}
     int jump(vector<int>& nums) {
-        memset(temp,-1,sizeof(temp));
-        return solve(0,nums);
+        vector<int>arr;
+        int maxi=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            maxi=max(nums[i]+i,maxi);
+            arr.push_back(maxi);
+        }
+        int count=0;
+        int i=0;
+        while(i<=nums.size())
+        {
+            if(arr[i]>=nums.size()-1)
+            {
+                
+                break;
+            }
+            count++;
+            int l=arr[i];
+            i=l;
+        }
+        if(i<nums.size()-1)
+        count++;
+        return count;
+
     }
 };
