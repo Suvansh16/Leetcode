6491508945faebedfbfc2fb6f20f6d9403586c31
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-09-25 02:46:38 +0530

    Time: 146 ms (78.3%), Space: 135 MB (71.29%) - LeetHub

--- 1522-stone-game-iii/1522-stone-game-iii.cpp
+++ 1522-stone-game-iii/1522-stone-game-iii.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-int n;
-int dp[50001];
-int solve(int i,vector<int>&v){
-    if(i>=n)
-    return 0;
-    if(dp[i]!=-1)
-    return dp[i];
-    int result=INT_MIN;
-    result=max(result,v[i]-solve(i+1,v));
-    if(i+1<n)
-    result=max(result,v[i]+v[i+1]-solve(i+2,v));
-    if(i+2<n)
-    result=max(result,v[i]+v[i+1] +v[i+2]-solve(i+3,v));
-    return dp[i]=result;
-}
+
     string stoneGameIII(vector<int>& stoneValue) {
-         memset(dp,-1,sizeof(dp));
-         n=stoneValue.size();
-        int val=solve(0,stoneValue);
-        if(val>0)
+        int n=stoneValue.size();
+        vector<int>dp(n+1,0);
+        for(int i=n-1;i>=0;i--)
+        {
+            if(i+1<=n)
+            dp[i]=stoneValue[i]-dp[i+1];
+            if(i+2<=n)
+            dp[i]=max(dp[i],stoneValue[i]+stoneValue[i+1]-dp[i+2]);
+            if(i+3<=n)
+            dp[i]=max(dp[i],stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-dp[i+3]);
+
+        }
+        int diff=dp[0];
+        if(diff>0)
         return "Alice";
-        if(val<0)
+        if(diff<0)
         return "Bob";
         return "Tie";
+        
     }
 };
