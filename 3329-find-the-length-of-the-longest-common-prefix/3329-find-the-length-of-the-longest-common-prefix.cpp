8c9f502bf42bec4819cf16da1fe49e04195ae079
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-09-27 01:31:59 +0530

    Time: 250 ms (73.37%), Space: 135.1 MB (85.82%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,49 +1,32 @@
-struct TrieNode {
-  vector<shared_ptr<TrieNode>> children;
-  TrieNode() : children(10) {}
-};
-
-class Trie {
- public:
-  void insert(const string& word) {
-    shared_ptr<TrieNode> node = root;
-    for (const char c : word) {
-      const int i = c - '0';
-      if (node->children[i] == nullptr)
-        node->children[i] = make_shared<TrieNode>();
-      node = node->children[i];
-    }
-  }
-
-  int search(const string& word) {
-    int prefixLength = 0;
-    shared_ptr<TrieNode> node = root;
-    for (const char c : word) {
-      const int i = c - '0';
-      if (node->children[i] == nullptr)
-        break;
-      node = node->children[i];
-      ++prefixLength;
-    }
-    return prefixLength;
-  }
-
- private:
-  shared_ptr<TrieNode> root = make_shared<TrieNode>();
-};
-
 class Solution {
- public:
-  int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-    int ans = 0;
-    Trie trie;
-
-    for (const int num : arr1)
-      trie.insert(to_string(num));
-
-    for (const int num : arr2)
-      ans = max(ans, trie.search(to_string(num)));
-
-    return ans;
-  }
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<int>st;
+        for(int i=0;i<arr1.size();i++)
+        {
+            int a=arr1[i];
+            while(a)
+            {
+                st.insert(a);
+                a/=10;
+            }
+        }
+        int maxans=0;
+        for(int i=0;i<arr2.size();i++)
+        {
+            int a=arr2[i];
+            while(a)
+            {
+                if(st.count(a))
+                {
+                    int val=floor(log10(a))+1;
+                    maxans=max(maxans,val);
+                    break;
+                }
+                a/=10;
+            }
+        
+        }
+        return maxans;
+    }
 };
