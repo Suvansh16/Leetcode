b23e3a75b1b11eace210026796d0a5ea25fde84e
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-10-05 15:12:08 +0530

    Time: 1001 ms (17.03%), Space: 194.9 MB (6.97%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
-void dfs(int a,unordered_map<int,int>&map,vector<int>&v1,vector<bool>&visited)
+void dfs(unordered_map<int,int>&map,vector<int>&v1,vector<bool>&visited,int a)
 {
-    
-    
     visited[a]=true;
-    int v=map[a];
-   if(v!=-1 && visited[v]==false)
-   {
-    visited[v]=true;
-    v1[v]=1+v1[a];
-    dfs(v,map,v1,visited);
-   }
-
+    int l=map[a];
+    if(l!=-1 && !visited[l]  )
+    {
+        visited[l]=true;
+        v1[l]=1+v1[a];
+        dfs(map,v1,visited,l);
+    }
 }
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n=edges.size();
-        vector<bool>visited(n,false);
+        vector<bool>visited1(n,false);
         vector<bool>visited2(n,false);
-        unordered_map<int,int>map;
-        int k=0;
-        for(int i:edges)
-        {
-            map[k]=i;
-            k++;
-        }
         vector<int>v1(n,INT_MAX);
         vector<int>v2(n,INT_MAX);
+        unordered_map<int,int>map;
+        for(int i=0;i<n;i++)
+        {
+            map[i]=edges[i];
+        }
         v1[node1]=0;
         v2[node2]=0;
-        dfs(node1,map,v1,visited);
-      
-        dfs(node2,map,v2,visited2);
-        int maxdist=INT_MAX;
-        int result=-1;
-        for(int i=0;i<v1.size();i++)
+        dfs(map,v1,visited1,node1);
+        dfs(map,v2,visited2,node2);
+        int maxans=INT_MAX;
+        int ind=-1;
+        for(int i=0;i<n;i++)
         {
-            int dist=max(v1[i],v2[i]);
-            if(maxdist>dist)
+            int t=max(v1[i],v2[i]);
+            if(maxans>t)
             {
-            maxdist=dist;
-            result=i;
+                maxans=t;
+                ind=i;
+
             }
         }
-       return result;
-        
-
+        return ind;
 
     }
 };
