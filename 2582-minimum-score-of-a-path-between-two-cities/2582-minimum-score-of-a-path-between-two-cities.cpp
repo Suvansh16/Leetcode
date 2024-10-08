bfc94f77694485094ef63b9ca92060561a5ac021
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-10-09 01:16:56 +0530

    Time: 344 ms (42.02%), Space: 140.6 MB (44.18%) - LeetHub

--- 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/2582-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,43 +1,39 @@
 class Solution {
 public:
-    int minScore(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<pair<int,int>>>map;
-        for(int i=0;i<roads.size();i++)
-        {
-            map[roads[i][0]].push_back({roads[i][1],roads[i][2]});
-            map[roads[i][1]].push_back({roads[i][0],roads[i][2]});
-        }
-        vector<int>ans(n+1,INT_MAX);
-        ans[1]=0;
-        queue<pair<int,int>>q;
-        q.push({1,0});
-        while(!q.empty())
-        {
-            int size=q.size();
-            while(size--)
-            {
-                pair<int,int>i=q.front();
-                q.pop();
-                int a=i.first;
-                int b=i.second;
-                for(pair<int,int>p:map[a])
-                {
-                    if(p.second<ans[p.first])
-                    {
-                        ans[p.first]=p.second;
-                        q.push({p.first,p.second});
+    int ans;
 
-                    }
-                }
+    void dfs(unordered_map<int, vector<pair<int, int>>>& map, int start, vector<bool>& visited) {
+        visited[start] = true;
+        
+        for (pair<int, int>& p : map[start]) {
+            int a = p.first;
+            int b = p.second;
+
+            // Update the minimum score
+            ans = min(ans, b);
+
+            // Continue DFS if the node has not been visited
+            if (!visited[a]) {
+                dfs(map, a, visited);
             }
         }
-        int mini=INT_MAX;
-        for(int i=0;i<=n;i++)
-        {
-            if(i==1)
-            continue;
-            mini=min(mini,ans[i]);
+    }
+
+    int minScore(int n, vector<vector<int>>& roads) {
+        unordered_map<int, vector<pair<int, int>>> map;
+
+        // Build the adjacency list
+        for (int i = 0; i < roads.size(); i++) {
+            map[roads[i][0]].push_back({roads[i][1], roads[i][2]});
+            map[roads[i][1]].push_back({roads[i][0], roads[i][2]});
         }
-        return mini;
+
+        vector<bool> visited(n + 1, false);
+        ans = INT_MAX;
+
+        // Start DFS from node 1
+        dfs(map, 1, visited);
+
+        return ans;
     }
 };
