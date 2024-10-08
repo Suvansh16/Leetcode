8a6d72beee1c2d127d171e4e14ff754564adfdd7
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-10-08 10:26:39 +0530

    Time: 23 ms (42.37%), Space: 18.7 MB (65.23%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,50 +1,55 @@
 class Solution {
 public:
-void BFS(vector<bool>&visited,int start,unordered_map<int,vector<int>>map)
-{
-    
-     queue<int>q;
-     q.push(start);
-   while(!q.empty())
-   {
-    
-    int a=q.front();
-     visited[a]=true;
-    q.pop();
-    
-        for(auto i:map[a])
-        {
-            if(!visited[i])
-            q.push(i);
+    vector<int> parent;
+    vector<int> rank;
+
+    // Find with path compression
+    int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    // Union by rank
+    void Union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY) return;
+
+        if (rank[rootX] > rank[rootY]) {
+            parent[rootY] = rootX;
+        } else if (rank[rootX] < rank[rootY]) {
+            parent[rootX] = rootY;
+        } else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
         }
-   }
-}
+    }
+
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n=isConnected[0].size();
-        unordered_map<int,vector<int>>map;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if (isConnected[i][j] == 1 && i != j) {
-                    map[i + 1].push_back(j + 1);  // Nodes are indexed 1-based
+        int n = isConnected.size();
+        parent.resize(n);
+        rank.resize(n, 0);
+
+        // Initialize the parent array
+        for (int i = 0; i < n; ++i) {
+            parent[i] = i;
+        }
+
+        // Union the connected nodes
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (isConnected[i][j] == 1 && find(i) != find(j)) {
+                    Union(i, j);
                 }
             }
-           
-        }
-        int count=0;
-        vector<bool>visited(n+1,false);
-        for(int i=1;i<=n;i++)
-        {
-            if(!visited[i])
-            {
-                BFS(visited,i,map);
-                  count++;
-
-            }
-          
         }
 
-        return count;
+        // Count the number of unique sets
+        unordered_set<int> uniqueParents;
+        for (int i = 0; i < n; ++i) {
+            uniqueParents.insert(find(i));  // Use find(i) to ensure the correct root
+        }
+
+        return uniqueParents.size();
     }
 };
