b9655dfb4bd21338f6866bc1694e9491a40b43c4
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-10-22 00:12:09 +0530

    Time: 6 ms (70.9%), Space: 14.4 MB (5.46%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
+void computeLPS(string pattern ,vector<int>&lps)
+{
+    int m=pattern.length();
+    int len=0;
+    lps[0]=0;
+    int i=1;
+    while(i<m)
+    {
+        if(pattern[i]==pattern[len])
+        {
+            len++;
+            lps[i]=len;
+            i++;
+        }
+        else
+        {
+            if(len!=0)
+            {
+                len=lps[len-1];
+            }
+            else
+            {
+                lps[i]=0;
+                i++;
+            }
+
+        }
+    }
+}
     string shortestPalindrome(string s) {
         string rev=s;
         reverse(rev.begin(),rev.end());
-        int n=s.length();
-        for(int i=0;i<s.length();i++)
-        {
-            
-            if(memcmp(s.c_str(),rev.c_str()+i,n-i)==0)
-            {
-                return rev.substr(0,i)+s;
-            }
-        }
-        return rev+s;
+        
+        string temp=s+"-"+rev;
+        
+        vector<int>LPS(temp.length(),0);
+        computeLPS(temp,LPS);
+        int longestLPSlength=LPS[temp.length()-1];
+        string culprit=rev.substr(0,s.length()-longestLPSlength);
+        return culprit+s;
     }
 };
