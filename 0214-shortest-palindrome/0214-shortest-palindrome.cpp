6185f870b8b3fadf43f29812d2a5b3034b2e9587
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-10-21 23:52:42 +0530

    Time: 18 ms (6.03%), Space: 11 MB (59.56%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.cpp
+++ 0214-shortest-palindrome/0214-shortest-palindrome.cpp
@@ -1,14 +1,17 @@
 class Solution {
- public:
-  string shortestPalindrome(string s) {
-    const string t = {s.rbegin(), s.rend()};
-    const string_view sv_s(s);
-    const string_view sv_t(t);
-
-    for (int i = 0; i < s.length(); ++i)
-      if (sv_s.substr(0, s.length() - i) == sv_t.substr(i))
-        return t.substr(0, i) + s;
-
-    return t + s;
-  }
+public:
+    string shortestPalindrome(string s) {
+        string rev=s;
+        reverse(rev.begin(),rev.end());
+        int n=s.length();
+        for(int i=0;i<s.length();i++)
+        {
+            
+            if(memcmp(s.c_str(),rev.c_str()+i,n-i)==0)
+            {
+                return rev.substr(0,i)+s;
+            }
+        }
+        return rev+s;
+    }
 };
