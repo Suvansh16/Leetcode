f957c8d364313e457f8fc3c6c9040c483c4f4c91
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-11-07 00:43:37 +0530

    Time: 79 ms (5.04%), Space: 32 MB (28.99%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -4,11 +4,15 @@ public:
         vector<int>temp=nums;
         for(int i=0;i<nums.size()-1;i++)
         {
+            bool swapped=false;
             for(int j=0;j<nums.size()-1-i;j++)
             {
                 if(nums[j]>nums[j+1] && __builtin_popcount(nums[j])==__builtin_popcount(nums[j+1]))
                 swap(nums[j],nums[j+1]);
+                swapped=true;
             }
+            if(swapped==false)
+            break;
         }
         for(int i=0;i<nums.size();i++)
         cout<<nums[i]<<" ";
