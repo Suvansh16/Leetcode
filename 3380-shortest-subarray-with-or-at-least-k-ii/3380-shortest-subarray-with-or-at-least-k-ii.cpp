2ee5a99e8c4f119a35804e5177dc6f181ab490eb
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-11-11 02:01:57 +0530

    Time: 63 ms (58.76%), Space: 88.3 MB (73.67%) - LeetHub

--- 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
+++ 3380-shortest-subarray-with-or-at-least-k-ii/3380-shortest-subarray-with-or-at-least-k-ii.cpp
@@ -1,39 +1,55 @@
 class Solution {
- public:
-  // Same as 3095. Shortest Subarray With OR at Least K I
-  int minimumSubarrayLength(vector<int>& nums, int k) {
-    constexpr int kMax = 50;
-    const int n = nums.size();
-    int ans = n + 1;
-    int ors = 0;
-    vector<int> count(kMax + 1);
+public:
+void addbit(vector<int>&v,int number)
+{
+    for(int i=0;i<32;i++)
+    {
+        if((number>>i) & 1)
+        {
+            v[i]++;
+        }
 
-    for (int l = 0, r = 0; r < n; r++) {
-      ors = orNum(ors, nums[r], count);
-      while (ors >= k && l <= r) {
-        ans = min(ans, r - l + 1);
-        ors = undoOrNum(ors, nums[l], count);
-        ++l;
-      }
     }
+}
+void removebit(vector<int>&v,int number)
+{
+    for(int i=0;i<32;i++)
+    {
+        if((number>>i)&1)
+        {
+            v[i]--;
+        }
+    }
+}
+int vectoint(vector<int>&v)
+{
+    int number=0;
+    for(int i=0;i<32;i++)
+    {
+        if(v[i]>0)
+        {
+            number|=(1<<i);
+        }
+    }
+    return number;
+}
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int i=0;
+        int j=0;
+        int ans=INT_MAX;
+        vector<int>v(32,0);
+        while(j<nums.size())
+        {
+            addbit(v,nums[j]);
+            while(i<=j && vectoint(v)>=k)
+            {
+                ans=min(ans,j-i+1);
+                removebit(v,nums[i]);
+                i++;
 
-    return (ans == n + 1) ? -1 : ans;
-  }
-
- private:
-  static constexpr int kMaxBit = 30;
-
-  int orNum(int ors, int num, vector<int>& count) {
-    for (int i = 0; i < kMaxBit; ++i)
-      if (num >> i & 1 && ++count[i] == 1)
-        ors += 1 << i;
-    return ors;
-  }
-
-  int undoOrNum(int ors, int num, vector<int>& count) {
-    for (int i = 0; i < kMaxBit; ++i)
-      if (num >> i & 1 && --count[i] == 0)
-        ors -= 1 << i;
-    return ors;
-  }
+            }
+            j++;
+        }
+        return ans==INT_MAX?-1:ans;
+    }
 };
