161fb86a8d9103ef72790c003406c7339aeabafd
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-11-14 22:58:18 +0530

    Time: 63 ms (64.16%), Space: 60.3 MB (61.15%) - LeetHub

---
+++ 2699-count-the-number-of-fair-pairs/2699-count-the-number-of-fair-pairs.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+int maxbinary(vector<int>&nums,int num,int upper,int lower,int presentind)
+{
+    int l=presentind;
+    int r=nums.size()-1;
+     int ans=-1;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(nums[mid]+num<=upper)
+        {
+            ans=mid;
+            l=mid+1;
+        }
+        else
+        {
+            r=mid-1;
+        }
+
+    }
+    return ans;
+}
+int minbinary(vector<int>&nums,int num,int upper,int lower,int presentind)
+{
+    int l=presentind+1;
+    int r=nums.size()-1;
+    int ans=-1;
+    while(l<=r)
+    {
+        int mid=(l+r)/2;
+        if(nums[mid]+num>=lower)
+        {
+            ans=mid;
+           r=mid-1;
+        }
+        else
+        {
+           l=mid+1;
+        }
+
+    }
+    return ans;
+}
+    long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        long long ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++)
+        {
+            int maxindex=maxbinary(nums,nums[i],upper,lower,i);
+            int minindex=minbinary(nums,nums[i],upper,lower,i);
+             if (maxindex != -1 && minindex != -1 && maxindex >= minindex) {
+                ans += (maxindex - minindex + 1);
+
+        }
+        }
+        return ans;
+    }
+};
