a9f059f3b9c0372ffd2ef63e4e849acf21e72780
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-01 00:18:02 +0530

    Time: 19 ms (8%), Space: 14.9 MB (15.04%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,32 +1,53 @@
+
+//Approach-2 (DP : O(n^2))
 class Solution {
 public:
-int solve(int start,vector<int>&prices,int stock,vector<vector<int>>&dp)
-{
-    if(start>=prices.size())
-    return 0;
-    if(dp[start][stock]!=-1)
-    return dp[start][stock];
-    int profit=INT_MIN;
-    int sell,skip_sell,take,skip_take;
-    if(stock)
-    {
-        sell=prices[start]+solve(start+2,prices,0,dp);
-        skip_sell=solve(start+1,prices,1,dp);
-        profit=max(sell,skip_sell);
+    int maxP(vector<int>& prices, int& n) {
+        if(n == 0 || n == 1)
+            return 0;
+        vector<int> t(n, 0);
+        
+        //t[i] = max profit at the end of ith day
+        t[0] = 0; //no profit on 0th day
+        t[1] = max(prices[1] - prices[0], 0); // max(bought_day_0 and sell_day_1, 0);
+        
+        /*  
+            Let's analyse ith day, i.e. t[i] :
+            i-th day can be :
+            - BUY DAY   -> NO, because why would someone buy on last day
+            
+            - COOL DOWN -> YES, in that case, max-profit will be previsou's day profit (t[i] = t[i-1])
+            
+            - SELL DAY  -> If we choose to sell today, We will have to select the best buy day from j = 0 to j = (i-1)
+                          So, profit = prices[j] - prices[i]; //i.e. we bought on day-j and sell on day-i
+                          But, what about previous-profit of j i.e. t[j]. We need to consider that as well to find whole
+                          maximum profit.
+                          So, for that let's look previously to j :
+                          Look at (j-1) -> It can't be buy-day, becuse we are already buying on jth day
+                                        -> It can't be sell-day because, after you sell your stock on (j-1)th day, you cannot buy stock on the next day i.e. (jth) day (i.e., cooldown one day).
+                                        -> It can only be cool-down day. max profit till (j-1)th day will be
+                                            equal to its privous profit i.e. t[j-2]
+                                            
+                        So, profit at day i = prices[j] - prices[i] + t[j-2];
+                          
+        */
+        
+        for(int i = 2; i<n; i++) {
+            t[i] = t[i-1]; //cool down
+            
+            //find best buy day from j  = 0 to i-1
+            for(int j = 0; j<=i-1; j++) {
+                
+                int prev_profit = j>=2 ? t[j-2] : 0;
+                
+                t[i] = max(t[i], prices[i] - prices[j] + prev_profit);
+            }
+        }
+        
+        return t[n-1];
     }
-
-    else
-    {
-        take=solve(start+1,prices,1,dp)-prices[start];
-        skip_take=solve(start+1,prices,0,dp);
-        profit=max(take,skip_take);
-    }
-
-    return dp[start][stock]=profit; 
-}
     int maxProfit(vector<int>& prices) {
-        int  stock=0;
-        vector<vector<int>>dp(prices.size()+1,vector<int>(2,-1));
-        return solve(0,prices,stock,dp);
+        int n = prices.size();
+        return maxP(prices, n);
     }
 };
