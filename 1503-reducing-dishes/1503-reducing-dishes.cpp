eca6c5ff3aff22c4e39b7bfa850621ee307e8611
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-03 22:48:57 +0530

    Time: 71 ms (6.38%), Space: 182.2 MB (5.04%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-int solve(vector<int>&s, int start,int time,vector<vector<int>>&dp)
-{
-    if(start>=s.size())
-    return 0;
-    if(dp[start][time]!=-1)
-    return dp[start][time];
-    int take=s[start]*time+solve(s,start+1,time+1,dp);
-    int nottake=solve(s,start+1,time,dp);
-    return dp[start][time]=max(take,nottake);
-
-}
     int maxSatisfaction(vector<int>& satisfaction) {
-        sort(satisfaction.begin(),satisfaction.end());
         int n=satisfaction.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(satisfaction,0,1,dp);
+          sort(satisfaction.begin(),satisfaction.end());
+        vector<vector<long long>>dp(501,vector<long long>(501,INT_MIN));
+        for(int i=0;i<501;i++)
+        {
+            dp[i][0]=0;
+        }
+        dp[0][1]=satisfaction[0];
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<n+1;j++)
+            {
+                long long cost1=satisfaction[i]*j+dp[i-1][j-1];
+                long long cost2=dp[i-1][j];
+                dp[i][j]=max(cost1,cost2);
+            }
+        }
+         long long  maxans=0;
+        for(int i=0;i<=n;i++)
+        {
+            maxans=max(maxans,dp[n-1][i]);
+        }
+        return maxans;
+       
     }
 };
