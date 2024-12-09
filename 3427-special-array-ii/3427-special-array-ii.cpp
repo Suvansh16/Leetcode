d851c2b3eec4905927b92dfed0b915b67019495b
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-10 00:59:50 +0530

    Time: 4 ms (87.5%), Space: 126.2 MB (76.85%) - LeetHub

--- 3427-special-array-ii/3427-special-array-ii.cpp
+++ 3427-special-array-ii/3427-special-array-ii.cpp
@@ -1,34 +1,52 @@
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-        vector<bool>ans;
-        vector<int>validindex;
-        int i=0;int j=0;
-        int n=nums.size();
-        while(i<n)
-        {
-            if(j<i)
-            {
-                j=i;
-            }
-            while(j+1<n && nums[j]%2 !=nums[j+1]%2)
-            {
-                j++;
-            }
-            validindex.push_back(j);
-            i++;
-        }
-        for(int i=0;i<queries.size();i++)
-        {
-            int a=queries[i][0];
-            int b=queries[i][1];
-            if(validindex[a]>=b)
-            ans.push_back(true);
-            else
-            ans.push_back(false);
+    bool bSearch(vector<int>& vi, int sp, int ep) {
+        int l = 0;
+        int r = vi.size()-1;
 
+        while(l <= r) {
+            int mid = l + (r-l)/2;
+
+            if(vi[mid] < sp) {
+                l = mid+1;
+            } else if(vi[mid] > ep) {
+                r = mid-1;
+            } else {
+                //we found violating index
+                return true;
+            }
         }
-        return ans;
+
+        return false;
+
+    }
+
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int m = queries.size();
+        int n = nums.size();
+
+        vector<int> vi; //violating indices ka list hai ye
+
+        for(int i = 1; i < n; i++) { //O(n)
+            if(nums[i]%2 == nums[i-1]%2) {
+                vi.push_back(i); //it will always be in sorted order
+            }
+        }
+
+        vector<bool> result(m, false);
+
+        for(int i = 0; i < m; i++) { //O(m)
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            bool hasViolatingIndexInRange = bSearch(vi, start+1, end); //log(n)
+
+            if(hasViolatingIndexInRange == false) { //Did not find any violating index
+                result[i] = true;
+            }
+        }
+
+        return result;
 
     }
 };
