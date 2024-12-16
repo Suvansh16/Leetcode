02ccfdd291634cf8dedd59c135b87b0f087522f6
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-17 00:51:09 +0530

    Time: 303 ms (5.21%), Space: 119.2 MB (5.06%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,42 +1,28 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        long long sum1=0;
-        long long sum2=0;
-        int i=0;
-        int n=piles.size();
-        int l=n-1;
-        int m=0;
-        while(i!=n)
-        {
-            if(i%2==0)
-            {
-                sum1+=max(piles[l],piles[m]);
-                if(piles[m]>=piles[l])
-                {
-                    m++;
-                    
-                }
-                else
-                {
-                    l--;
-                }
-            }
-            else{
-               sum2+=min(piles[l],piles[m]);
-                if(piles[m]>piles[l])
-                {
-                    m++;
-                    
-                }
-                else
-                {
-                    l--;
-                }
+bool solve(int i,int j,int l,int alice_sum,int bob_sum,vector<int>&piles,vector<vector<vector<int>>>&dp)
+{
+    if(i>j)
+    return alice_sum>bob_sum;
+    if(dp[i][j][l]!=-1)
+    return dp[i][j][l];
+    bool a=false,b=false,c=false,d=false;
+    if(l==0)
+    {
+       
 
-            }
-            i++;
-        }
-        return sum1>sum2;
+        a=solve(i+1,j,1,alice_sum+piles[i],bob_sum,piles,dp);
+        b=solve(i,j-1,1,alice_sum+piles[j],bob_sum,piles,dp);
+    }
+    if(l==1)
+    {
+        c=solve(i+1,j,0,alice_sum,bob_sum+piles[i],piles,dp);
+       d=solve(i,j-1,0,alice_sum,bob_sum+piles[j],piles,dp);
+    }
+    return dp[i][j][l]=a || b || c || d ;
+}
+    bool stoneGame(vector<int>& piles) {
+        vector<vector<vector<int>>>dp(piles.size(),vector<vector<int>>(piles.size(),vector<int>(2,-1)));
+        return solve(0,piles.size()-1,0,0,0,piles,dp);
     }
 };
