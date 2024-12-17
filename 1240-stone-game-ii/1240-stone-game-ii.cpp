c617f2184548142c8de93ff0df4c84bedd21942e
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-17 23:41:30 +0530

    Time: 14 ms (49.03%), Space: 11.1 MB (75.72%) - LeetHub

--- 1240-stone-game-ii/1240-stone-game-ii.cpp
+++ 1240-stone-game-ii/1240-stone-game-ii.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
 int n;
-int t[101][2][101];
-int solve(int idx,int turn,int m,vector<int>&piles)
+int dp[101][101][2];
+int solve(int start,vector<int>&piles,int m,int turn)
 {
-    if(idx>=n)
+    if(start>=piles.size())
     return 0;
-    if(t[idx][turn][m]!=-1)
-    return t[idx][turn][m];
-
-    int result=(turn==1?-1:INT_MAX);
+    if(dp[start][m][turn]!=-1)
+    return dp[start][m][turn];
+    int result=(turn==1)?-1:INT_MAX;
     int stones=0;
-    for(int i=1;i<=min(2*m,n-idx);i++)
+
+    for(int i=1;i<=min(2*m,n-start);i++)
     {
-        stones+=piles[idx+i-1];
+        stones+=piles[start+i-1];
         if(turn==1)
         {
-            result=max(result,stones+solve(i+idx,0,max(m,i),piles));
+            result=max(result,stones+solve(i+start,piles,max(m,i),0));
+
         }
         else
         {
-            result=min(result,solve(i+idx,1,max(m,i),piles));
+            result=min(result,solve(i+start,piles,max(m,i),1));
         }
     }
-    return t[idx][turn][m]=result;
+    return dp[start][m][turn]=result;
 }
     int stoneGameII(vector<int>& piles) {
         n=piles.size();
-        memset(t,-1,sizeof(t));
-        return solve(0,1,1,piles);
+        memset(dp,-1,sizeof(dp));
+        return solve(0,piles,1,1);
     }
 };
