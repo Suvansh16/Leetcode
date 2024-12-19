27cdb8e34e4006ae4091a7ce86f2f2adec38418f
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-20 00:18:39 +0530

    Time: 0 ms (100%), Space: 9 MB (42.82%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int cumulative_sum=0;
-        int cumsum_orig=0;
-        int cunk=0;
+        int maxtillnow=-1;
+        int count=0;
         for(int i=0;i<arr.size();i++)
         {
-            cumulative_sum+=arr[i];
-            cumsum_orig+=i;
-            if(cumulative_sum==cumsum_orig)
-            cunk++;
+            maxtillnow=max(maxtillnow,arr[i]);
+            if(maxtillnow==i)
+            count++;
         }
-        return cunk;
+        return count;
     }
 };
