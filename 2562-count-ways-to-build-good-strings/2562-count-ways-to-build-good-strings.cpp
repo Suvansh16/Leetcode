dc0a4590f02d5ec325a5a60307b33a8363d0f7e9
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2024-12-30 20:56:56 +0530

    Time: 15 ms (7.24%), Space: 33.4 MB (5.87%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
+int l,h,z,o;
 const int mod=1e9+7;
+int solve(int length,vector<int>&dp)
+{
+    if(length>h)
+    return 0;
+    if(dp[length]!=-1)
+    return dp[length];
+    int add=0;
+    if(length>=l && length<=h)
+    add=1;
+    int m=solve(length+o,dp);
+    int n=solve(length+z,dp);
+    return dp[length]=(add+m+n)%mod;
+
+}
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int>dp(high+1,0);
-        dp[0]=1;
-        for(int i=1;i<=high;i++)
-        {
-             if(i-zero>=0)
-            dp[i]=(dp[i]%mod+dp[i-zero]%mod)%mod;
-            if(i-one>=0)
-            {
-                dp[i]=(dp[i]%mod+dp[i-one]%mod)%mod;
-            }
-           
-        }
-        int ans=0;
-        for(int i=low;i<=high;i++)
-        {
-            ans=(ans%mod+dp[i]%mod)%mod;
-        }
-        return ans;
+        l=low;
+        h=high;
+        z=zero;
+        o=one;
+        vector<int>dp(1e5+1,-1);
+        return solve(0,dp);
     }
 };
