eb2bfc1712521000bd1c81a8259e699a62e20a6c
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-01-06 18:01:17 +0530

    Time: 0 ms (100%), Space: 12.2 MB (50.97%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -1,50 +1,26 @@
 class Solution {
 public:
     vector<int> minOperations(string boxes) {
-        vector<int>left_balls;
-        vector<int>right_balls;
+        vector<int>ans(boxes.length(),0);
         int n=boxes.length();
-        
+        int sum=0;
+        int count=0;
         for(int i=0;i<n;i++)
         {
-            if(i==0)
-            left_balls.push_back(0);
-            else
-            {
-                int sum=0;
-                for(int j=0;j<i;j++)
-                {
-                    if(boxes[j]=='1')
-                    {
-                        sum+=abs(j-i);
-                    }
-                }
-                left_balls.push_back(sum);
-            }
+            ans[i]=ans[i]+sum;
+            if(boxes[i]=='1')
+            count++;
+            sum+=count;
         }
-        for(int i=0;i<n;i++)
+        sum=0;
+        count=0;
+        for(int i=n-1;i>=0;i--)
         {
-            if(i==n-1)
-            right_balls.push_back(0);
-            else
-            {
-                int sum=0;
-                for(int j=i+1;j<n;j++)
-                {
-                    if(boxes[j]=='1')
-                    {
-                         sum+=abs(j-i);
-                    }
-                }
-                right_balls.push_back(sum);
-            }
-        }
-        vector<int>ans;
-        for(int i=0;i<n;i++)
-        {
-            ans.push_back(left_balls[i]+right_balls[i]);
+            ans[i]+=sum;
+            if(boxes[i]=='1')
+            count++;
+            sum+=count;
         }
         return ans;
-
     }
 };
