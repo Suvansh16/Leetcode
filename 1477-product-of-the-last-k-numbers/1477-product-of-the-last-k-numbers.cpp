b73ee9f18f7e22927755246a82014d2f53f2865e
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-02-15 14:21:17 +0530

    Time: 15 ms (85.98%), Space: 77.8 MB (93.39%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,27 +1,32 @@
 class ProductOfNumbers {
 public:
-    vector<int> prefixProd;  // Stores cumulative products
-    
+vector<int>vec;
     ProductOfNumbers() {
-        prefixProd.clear();
+        vec.clear();
     }
     
     void add(int num) {
-        if (num == 0) {
-            // Reset the prefix product list when encountering zero
-            prefixProd.clear();
-        } else {
-            if (prefixProd.empty()) {
-                prefixProd.push_back(num);
-            } else {
-                prefixProd.push_back(prefixProd.back() * num);
-            }
-        }
+        if(num==0)
+        vec.clear();
+        else if(vec.empty())
+        vec.push_back(num);
+        else
+        vec.push_back(vec.back()*num);
     }
     
     int getProduct(int k) {
-        if (k > prefixProd.size()) return 0;  // If k exceeds available elements, return 0
-        if (k == prefixProd.size()) return prefixProd.back();
-        return prefixProd.back() / prefixProd[prefixProd.size() - k - 1];
+        if(k>vec.size())
+        return 0;
+        if(k==vec.size())
+        return vec.back();
+        else
+        return vec.back()/vec[vec.size()-k-1];
     }
 };
+
+/**
+ * Your ProductOfNumbers object will be instantiated and called as such:
+ * ProductOfNumbers* obj = new ProductOfNumbers();
+ * obj->add(num);
+ * int param_2 = obj->getProduct(k);
+ */
