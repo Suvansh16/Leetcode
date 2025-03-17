624cffd230ba5f1ca8c0f2f8474f0c9981dc805f
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-03-17 14:34:21 +0530

    Time: 3 ms (20.12%), Space: 16.7 MB (5.02%) - LeetHub

--- 0871-keys-and-rooms/0871-keys-and-rooms.cpp
+++ 0871-keys-and-rooms/0871-keys-and-rooms.cpp
@@ -1,32 +1,65 @@
 class Solution {
+
 public:
-    bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        int n=rooms.size();
-        vector<bool>visited(n,false);
-        queue<vector<int>>q;
-        q.push(rooms[0]);
-        visited[0]=true;
-        while(!q.empty())
-        {
-           
-            vector<int>a=q.front();
-            q.pop();
-             int size=a.size();
-            while(size--)
-            {
-                
-                if(!visited[a[size]])
-                {
-                    q.push(rooms[a[size]]);
-                    visited[a[size]]=true;
-                }
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(visited[i]==false)
-            return false;
-        }
-        return true;
+
+  void solve(int start, unordered_map<int, vector<int>>& map, vector<bool>& visited) {
+
+    visited[start] = true;
+
+    for (auto i : map[start]) {
+
+      if (!visited[i]) { // Fix this line
+
+        solve(i, map, visited);
+
+      }
+
     }
+
+  }
+
+   
+
+  bool canVisitAllRooms(vector<vector<int>>& rooms) {
+
+    unordered_map<int, vector<int>> map;
+
+    for (int i = 0; i < rooms.size(); i++) {
+
+      vector<int> v = rooms[i];
+
+      map[i] = v;
+
+    }
+
+     
+
+    int n = rooms.size();
+
+    vector<bool> visited(n, false);
+
+     
+
+    solve(0, map, visited);
+
+     
+
+    // Check if all rooms are visited
+
+    for (int i = 0; i < n; i++) {
+
+      if (visited[i] == false) {
+
+        return false;
+
+      }
+
+    }
+
+     
+
+    return true;
+
+  }
+
 };
