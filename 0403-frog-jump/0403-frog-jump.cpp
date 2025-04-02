46222f119881ce5ccfa99a67ff1edd20197490d9
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-02 18:38:45 +0530

    Time: 41 ms (90.12%), Space: 30 MB (77.8%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    unordered_map<int, unordered_set<int>> dp;
-
-    bool solve(int i, int walk, vector<int>& stones, unordered_map<int, int>& index) {
-        if (i == stones.size() - 1) return true;
-        if (dp[i].count(walk)) return false; // Memoization: avoid recomputation
-        dp[i].insert(walk);
-
-        for (int jump : {walk - 1, walk, walk + 1}) {
-            if (jump > 0 && index.count(stones[i] + jump)) {
-                if (solve(index[stones[i] + jump], jump, stones, index)) {
-                    return true;
-                }
-            }
+unordered_map<int,unordered_set<int>>dp;
+bool solve(int start,int walk,vector<int>&stones,unordered_map<int,int>&index)
+{
+    if(start==stones.size()-1)
+    return true;
+    if(dp[start].count(walk))
+    return false;
+    dp[start].insert(walk);
+    for(auto jump:{walk-1,walk,walk+1})
+    {
+        if(jump>0 && index.count(stones[start]+jump))
+        {
+            if(solve(index[stones[start]+jump],jump,stones,index))
+            return true;
         }
-        return false;
     }
-
+    return false;
+}
     bool canCross(vector<int>& stones) {
-        if (stones[1] != 1) return false; // First jump must be 1
-        unordered_map<int, int> index;  // Maps stone value to index
-        for (int i = 0; i < stones.size(); i++) {
-            index[stones[i]] = i;
+        if(stones[1]!=1)
+        return false;
+        
+        unordered_map<int,int>index;
+        for(int i=0;i<stones.size();i++)
+        {
+            index[stones[i]]=i;
         }
-        return solve(0, 0, stones, index);
+        return solve(0,0,stones,index);
     }
 };
