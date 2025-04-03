4395fdfc5b55394e5825886632a697c701999b40
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-03 19:04:26 +0530

    Time: 0 ms (100%), Space: 69.2 MB (33.28%) - LeetHub

--- 2591-frog-jump-ii/2591-frog-jump-ii.cpp
+++ 2591-frog-jump-ii/2591-frog-jump-ii.cpp
@@ -2,6 +2,10 @@ class Solution {
 public:
     int maxJump(vector<int>& stones) {
         int ans=stones[1]-stones[0];
+
+        //frog will move alternatively to get minimum diff 
+        // we have to find two path and the two path will not intrract with each other
+        
         for(int i=2;i<stones.size();i++)
         {
             ans=max(ans,stones[i]-stones[i-2]);
