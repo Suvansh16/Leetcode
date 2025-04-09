80f0bcc5f151e61dbf9e2097e280ac99fb1bbe2c
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-09 12:00:07 +0530

    Time: 0 ms (100%), Space: 15.6 MB (21.29%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+vector<vector<int>>ans;
+void solve(int start,vector<int>&nums,vector<int>output)
+{
+    if(start>=nums.size())
+    {
+        ans.push_back(output);
+        return ;
+    }
+
+        if(output.size()==0 || nums[start]!=nums[start-1]){
+        output.push_back(nums[start]);
+        solve(start+1,nums,output);
+        output.pop_back();
+        }
+        solve(start+1,nums,output);
+
+
+}
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>output;
+        sort(nums.begin(),nums.end());
+        solve(0,nums,output);
+        return ans;
+    }
+};
