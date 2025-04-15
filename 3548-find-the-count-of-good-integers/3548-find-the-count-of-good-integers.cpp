48b1af41861d5fc87c32b3ce66d0d65e3aa62ea5
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-16 00:24:35 +0530

    Time: 443 ms (62.21%), Space: 22.2 MB (87.7%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
@@ -1,58 +1,55 @@
 class Solution {
 public:
+
+
     long long countGoodIntegers(int n, int k) {
-        unordered_set<string> st;
+        int d=(n+1)/2;
+        long long start=pow(10,d-1);
+        long long end=pow(10,d)-1;
+        unordered_set< string >st;
+        vector<long long>factorial(11,1);
+    for(int i=1;i<11;i++)
+    {
+    factorial[i]=factorial[i-1]*i;
+    }
+        for(long long i=start;i<=end;i++)
+        {
+            string left=to_string(i);
+            string full="";
+            string right;
+            if(n%2==0)
+            right=left;
+            else
+            right=left.substr(0,left.size()-1);
+            reverse(right.begin(),right.end());
+            full=left+right;
 
-        int d = (n+1)/2;
-        int start = pow(10, d-1);
-        int end   = pow(10, d) - 1;
-
-        for(int num = start; num <= end; num++) {
-            string leftHalf = to_string(num);
-            string full = "";
-
-            if(n%2 == 0) {
-                string rightHalf = leftHalf;
-                reverse(rightHalf.begin(), rightHalf.end());
-                full = leftHalf + rightHalf;
-            } else {
-                string rightHalf = leftHalf.substr(0, d-1);
-                reverse(rightHalf.begin(), rightHalf.end());
-                full = leftHalf + rightHalf;
-            }
-
-            long long number = stoll(full);
-            if(number % k != 0)
-                continue;
-            
-            sort(full.begin(), full.end());
+            long long t=stoll(full);
+            if(t%k!=0)
+            continue;
+            sort(full.begin(),full.end());
             st.insert(full);
-        }
 
-        vector<long long> factorial(11, 1);
-        for(int i = 1; i < 11; i++) {
-            factorial[i] = factorial[i-1] * i;
         }
-        long long result = 0;
-        for(const string &s : st) {
-            vector<int> count(10, 0);
-            for(const char &ch : s) {
-                count[ch - '0']++; //mp[ch]++
+        long long result=0;
+        for(const string  &i:st)
+        {
+            vector<int>count(10,0);
+            for(const char &t:i)
+            {
+                count[t-'0']++;
             }
-
-            int totalDigits = s.length();
-            int zeroDigits = count[0];
-            int nonZeroDigits = totalDigits - zeroDigits;
-
-            long long perm = (nonZeroDigits * factorial[totalDigits-1]);
-
-            for(int i = 0; i < 10; i++) {
-                perm /= factorial[count[i]];
+            int totaldigits=i.length();
+            int totalnonzerodigits=i.size()-count[0];
+            int zero=count[0];
+            long long perm=totalnonzerodigits*factorial[totaldigits-1];
+            for(int i=0;i<10;i++)
+            {
+                perm/=factorial[count[i]];
             }
-
-            result += perm;
+            result+=perm;
         }
-
         return result;
+
     }
 };
