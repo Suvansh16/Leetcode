8ae3117ea56a223d1e722f2d2200c870bf353104
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-16 00:22:24 +0530

    Time: 456 ms (59.48%), Space: 22.4 MB (80.07%) - LeetHub

--- 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
+++ 3548-find-the-count-of-good-integers/3548-find-the-count-of-good-integers.cpp
@@ -1,59 +1,58 @@
-#define ll long long
 class Solution {
-    ll vectorToNumber(const vector<int>& nums) {
-        ll res = 0;
-        for (int dig : nums) res = res * 10 + dig;
-        return res;
-    }
-
-    ll fact(int total, ll f = 1) {
-        for (int i = 2; i <= total; i++) f *= i;
-        return f;
-    }
-
-    ll totalPermutations(map<int, int>& mpp, int total) {
-        ll totalDig = fact(total);
-        for (auto& palinVal : mpp) totalDig /= fact(palinVal.second);
-        return totalDig;
-    }
-
-    ll permsWithZero(map<int, int> mpp, int total) {
-        return mpp[0] == 0 ? 0 : ([&]() {
-            mpp[0]--;
-            ll tot = fact(total - 1);
-            for (auto& palinVal : mpp)
-                tot /= fact(palinVal.second);
-            return tot;
-        }());
-    }
-
 public:
-    ll res = 0;
-    set<map<int, int>> visited;
+    long long countGoodIntegers(int n, int k) {
+        unordered_set<string> st;
 
-    void genPal(vector<int>& palin, int left, int right, int divisor, int total) {
-        if (left > right) {
-            ll palinVal = vectorToNumber(palin);
-            if (palinVal % divisor == 0) {
-                map<int, int> digMpp;
-                for (ll result = palinVal; result; result /= 10)
-                    digMpp[result % 10]++;
-                if (!visited.count(digMpp)) {
-                    res += totalPermutations(digMpp, total) - permsWithZero(digMpp, total);
-                    visited.insert(digMpp);
-                }
+        int d = (n+1)/2;
+        int start = pow(10, d-1);
+        int end   = pow(10, d) - 1;
+
+        for(int num = start; num <= end; num++) {
+            string leftHalf = to_string(num);
+            string full = "";
+
+            if(n%2 == 0) {
+                string rightHalf = leftHalf;
+                reverse(rightHalf.begin(), rightHalf.end());
+                full = leftHalf + rightHalf;
+            } else {
+                string rightHalf = leftHalf.substr(0, d-1);
+                reverse(rightHalf.begin(), rightHalf.end());
+                full = leftHalf + rightHalf;
             }
-            return;
-        }
-        for (int dig = (left == 0 ? 1 : 0); dig <= 9; dig++) {
-            palin[left] = palin[right] = dig;
-            genPal(palin, left + 1, right - 1, divisor, total);
-        }
-    }
 
-    ll countGoodIntegers(int total, int divisor) {
-        vector<int> palin(total);
-        genPal(palin, 0, total - 1, divisor, total);
-        return res;
+            long long number = stoll(full);
+            if(number % k != 0)
+                continue;
+            
+            sort(full.begin(), full.end());
+            st.insert(full);
+        }
+
+        vector<long long> factorial(11, 1);
+        for(int i = 1; i < 11; i++) {
+            factorial[i] = factorial[i-1] * i;
+        }
+        long long result = 0;
+        for(const string &s : st) {
+            vector<int> count(10, 0);
+            for(const char &ch : s) {
+                count[ch - '0']++; //mp[ch]++
+            }
+
+            int totalDigits = s.length();
+            int zeroDigits = count[0];
+            int nonZeroDigits = totalDigits - zeroDigits;
+
+            long long perm = (nonZeroDigits * factorial[totalDigits-1]);
+
+            for(int i = 0; i < 10; i++) {
+                perm /= factorial[count[i]];
+            }
+
+            result += perm;
+        }
+
+        return result;
     }
 };
