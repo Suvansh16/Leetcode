7ad18894c46e8d79639f990759331ec76adda6d9
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-23 11:20:07 +0530

    Time: 75 ms (9.72%), Space: 50 MB (18.45%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-
-int solve(vector<int>&coins,int amount,vector<int>&dp)
+int solve(int start,int amount,vector<int>&coins,vector<vector<int>>&dp)
 {
-    if(amount<0)
-    return INT_MAX;
     if(amount==0)
     return 0;
-    if(dp[amount]!=-1)
-    return dp[amount];
-    int ans=INT_MAX;
-    for(int i=0;i<coins.size();i++)
+    if(start>=coins.size() || amount<0)
+    return INT_MAX;
+    if(dp[start][amount]!=-1)
+    return dp[start][amount];
+    int take=INT_MAX,nottake=INT_MAX;
+    nottake=solve(start+1,amount,coins,dp);
+    if(solve(start,amount-coins[start],coins,dp)!=INT_MAX)
     {
-        int a=solve(coins,amount-coins[i],dp);
-        if(a!=INT_MAX)
-        {
-            ans=min(ans,a+1);
-        }
+        take=1+solve(start,amount-coins[start],coins,dp);
     }
-    return dp[amount]=ans;
+    return dp[start][amount]=min(take,nottake);
 }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,-1);
-        int val=solve(coins,amount,dp);
-        return val==INT_MAX?-1:val;
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
+       int val=solve(0,amount,coins,dp);
+        if(val==INT_MAX)
+        return -1;
+        return val;
     }
 };
