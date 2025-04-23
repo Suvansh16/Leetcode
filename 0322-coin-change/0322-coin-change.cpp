f224e91ea13d1c34635cd78bb958cccd9075d04e
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-04-23 11:37:01 +0530

    Time: 46 ms (35.51%), Space: 48.7 MB (41.62%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-int solve(int start,int amount,vector<int>&coins,vector<vector<int>>&dp)
-{
-    if(amount==0)
-    return 0;
-    if(start>=coins.size() || amount<0)
-    return INT_MAX;
-    if(dp[start][amount]!=-1)
-    return dp[start][amount];
-    int take=INT_MAX,nottake=INT_MAX;
-    nottake=solve(start+1,amount,coins,dp);
-    if(solve(start,amount-coins[start],coins,dp)!=INT_MAX)
-    {
-        take=1+solve(start,amount-coins[start],coins,dp);
-    }
-    return dp[start][amount]=min(take,nottake);
-}
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
-       int val=solve(0,amount,coins,dp);
-        if(val==INT_MAX)
-        return -1;
-        return val;
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,INT_MAX));
+        for(int i=0;i<coins.size();i++)
+        {
+
+            dp[i][0]=0;
+        }
+         for (int j = 1; j <= amount; j++) {
+            if (j % coins[0] == 0) {
+                dp[0][j] = j / coins[0]; // Only use the first coin repeatedly
+            }
+        }
+        for(int i=1;i<coins.size();i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+                int take=INT_MAX,nottake=INT_MAX;
+                 nottake=dp[i-1][j];
+            if(j - coins[i] >= 0 && dp[i][j-coins[i]]!=INT_MAX)
+            {
+            take=1+ dp[i][j-coins[i]];
+            }
+           dp[i][j]=min(take,nottake);
+            }
+        }
+        return dp[coins.size()-1][amount]==INT_MAX?-1:dp[coins.size()-1][amount];
     }
 };
