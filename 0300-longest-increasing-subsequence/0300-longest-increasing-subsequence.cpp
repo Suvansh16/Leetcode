5b053fb8fef49bfa4b09eeca1d0c914eb37ccd89
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-05-16 18:52:13 +0530

    Time: 68 ms (54.31%), Space: 14.5 MB (45.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int  n=nums.size();
-        int LIS=1;
+        int n=nums.size();
         vector<int>dp(n,1);
-        for(int i=0;i<nums.size();i++)
+        vector<int>parent(n,-1);
+        int LIS=1;
+        for(int i=0;i<n;i++)
         {
             for(int j=0;j<=i-1;j++)
             {
-                if(nums[i]>nums[j])
+                if(nums[i]>nums[j] && dp[i]<(dp[j]+1))
                 {
-                    dp[i]=max(dp[i],dp[j]+1);
+                    dp[i]=dp[j]+1;
                     LIS=max(LIS,dp[i]);
+                    parent[i]=j;
                 }
             }
         }
+        //find the LIS index
+        int ind=-1;
+        vector<int>result;
+        for(int i=0;i<n;i++)
+        {
+            if(dp[i]==LIS)
+            ind=i;
+        }
+        while(ind!=-1)
+        {
+            result.push_back(nums[ind]);
+            ind=parent[ind];
+        }
+        reverse(result.begin(),result.end());
+        for(int i:result)
+        cout<<i<<" ";
         return LIS;
-
     }
 };
