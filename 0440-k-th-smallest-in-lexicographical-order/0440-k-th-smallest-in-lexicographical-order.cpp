ac14887babeaffde7f4105e046fe2c4f0c8a9f42
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-06-09 16:27:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (48.71%) - LeetHub

--- 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
+++ 0440-k-th-smallest-in-lexicographical-order/0440-k-th-smallest-in-lexicographical-order.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    int countPrefix(long prefix, int n) {
-        long curr = prefix, next = prefix + 1;
-        long count = 0;
-        while (curr <= n) {
-            count += min((long)n + 1, next) - curr;
-            curr *= 10;
-            next *= 10;
-        }
-        return count;
+int countprefix(long prefix,int n)
+{
+    long curr=prefix,next=prefix+1;
+    long count=0;
+    while(curr<=n)
+    {
+        count+=min((long)n+1,next)-curr;
+        curr*=10;
+        next*=10;
     }
-
+    return count;
+}
     int findKthNumber(int n, int k) {
-        int curr = 1;
-        k--; // because we start from 1 already
-
-        while (k > 0) {
-            int count = countPrefix(curr, n);
-            if (count <= k) {
-                // Skip this whole subtree
-                k -= count;
-                curr += 1;
-            } else {
-                // Go deeper in the tree
-                curr *= 10;
-                k -= 1;
+        int curr=1;
+        k--;
+        while(k>0)
+        {
+            int count=countprefix(curr,n);
+            if(count<=k)
+            {
+                k-=count;
+                curr+=1;
+            }
+            else
+            {
+                curr*=10;
+                k-=1;
             }
         }
         return curr;
+
     }
 };
