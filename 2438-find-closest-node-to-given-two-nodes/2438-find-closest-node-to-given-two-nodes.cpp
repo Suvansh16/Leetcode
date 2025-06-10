e4fbcd842cacd36d7953ca7bc8a331cf882fb2ab
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-06-10 18:42:25 +0530

    Time: 369 ms (5.15%), Space: 178.1 MB (5.16%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-void dfs(unordered_map<int,int>&map,vector<int>&v1,vector<bool>&visited,int a)
-{
-    visited[a]=true;
-    int l=map[a];
-    if(l!=-1 && !visited[l]  )
-    {
-        visited[l]=true;
-        v1[l]=1+v1[a];
-        dfs(map,v1,visited,l);
+    void dfs(unordered_map<int, int>& dist, int start, int dis, unordered_set<int>& visited, vector<int>& edges) {
+        if (start == -1 || visited.count(start)) return;
+        dist[start] = dis;
+        visited.insert(start);
+        dfs(dist, edges[start], dis + 1, visited, edges);
     }
-}
-    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n=edges.size();
-        vector<bool>visited1(n,false);
-        vector<bool>visited2(n,false);
-        vector<int>v1(n,INT_MAX);
-        vector<int>v2(n,INT_MAX);
-        unordered_map<int,int>map;
-        for(int i=0;i<n;i++)
-        {
-            map[i]=edges[i];
-        }
-        v1[node1]=0;
-        v2[node2]=0;
-        dfs(map,v1,visited1,node1);
-        dfs(map,v2,visited2,node2);
-        int maxans=INT_MAX;
-        int ind=-1;
-        for(int i=0;i<n;i++)
-        {
-            int t=max(v1[i],v2[i]);
-            if(maxans>t)
-            {
-                maxans=t;
-                ind=i;
 
+    int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+        unordered_map<int, int> dist1, dist2;
+        unordered_set<int> visited1, visited2;
+
+        dfs(dist1, node1, 0, visited1, edges);
+        dfs(dist2, node2, 0, visited2, edges);
+
+        int ans = -1;
+        int minDist = INT_MAX;
+
+        for (int i = 0; i < edges.size(); ++i) {
+            if (dist1.count(i) && dist2.count(i)) {
+                int maxDist = max(dist1[i], dist2[i]);
+                if (maxDist < minDist || (maxDist == minDist && i < ans)) {
+                    minDist = maxDist;
+                    ans = i;
+                }
             }
         }
-        return ind;
 
+        return ans;
     }
 };
