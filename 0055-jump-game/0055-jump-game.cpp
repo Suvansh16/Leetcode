c328a5d87277f07396a195362a9efe8a9396cd56
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-07-08 15:01:39 +0530

    Time: 4 ms (24.33%), Space: 52.4 MB (19.86%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        vector<bool> dp(nums.size(), false);
-        dp[nums.size() - 1] = true; // Can always reach end from the end
-
-        for (int i = nums.size() - 2; i >= 0; i--) {
-            int farthest = min((int)nums.size() - 1, i + nums[i]);
-            for (int j = i + 1; j <= farthest; j++) {
-                if (dp[j]) {
-                    dp[i] = true;
-                    break;
-                }
-            }
+        int maxReach = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i > maxReach) return false;
+            maxReach = max(maxReach, i + nums[i]);
         }
-        return dp[0];
+        return true;
     }
 };
