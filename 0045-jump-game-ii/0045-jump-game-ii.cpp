fda3cc70c46770a1c64c86ace4569500c0cc8772
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-07-09 14:20:58 +0530

    Time: 72 ms (18.23%), Space: 21.5 MB (26.57%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
-    int solve(int start, vector<int>& nums, vector<int>& dp) {
-        if (start >= nums.size() - 1) return 0;
-        if (dp[start] != -1) return dp[start];
-
-        int ans = INT_MAX;
-        for (int i = start + 1; i <= min((int)nums.size() - 1, start + nums[start]); i++) {
-            int t = solve(i, nums, dp);
-            if (t != INT_MAX)
-                ans = min(ans, t + 1);
-        }
-        return dp[start] = ans;
-    }
-
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return solve(0, nums, dp);
+        vector<int>dp(nums.size()+1,INT_MAX);
+        dp[0]=0;
+        for(int i=0;i<nums.size();i++)
+        {
+
+            for(int j=i+1;j<=min((int)nums.size()-1,i+nums[i]);j++)
+            {
+                dp[j]=min(dp[j],1+dp[i]);
+            }
+        }
+        return dp[nums.size()-1];
     }
 };
