706cebf4fe30889e678f58b3999b9633388128a7
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-07-19 00:40:14 +0530

    Time: 184 ms (9.06%), Space: 168.5 MB (5.03%) - LeetHub

--- 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/2438-find-closest-node-to-given-two-nodes.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    void dfs(unordered_map<int, int>& dist, int start, int dis, unordered_set<int>& visited, vector<int>& edges) {
-        if (start == -1 || visited.count(start)) return;
-        dist[start] = dis;
-        visited.insert(start);
-        dfs(dist, edges[start], dis + 1, visited, edges);
-    }
-
+void solve(int node,unordered_map<int,int>&map,vector<bool>&visited,vector<int>&v)
+{   visited[node]=true;
+    int t=map[node];
+    if(t!=-1 && !visited[t])
+    {
+        v[t]=1+v[node];
+        solve(t,map,visited,v);
+    } 
+}
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        unordered_map<int, int> dist1, dist2;
-        unordered_set<int> visited1, visited2;
-
-        dfs(dist1, node1, 0, visited1, edges);
-        dfs(dist2, node2, 0, visited2, edges);
-
-        int ans = -1;
-        int minDist = INT_MAX;
-
-        for (int i = 0; i < edges.size(); ++i) {
-            if (dist1.count(i) && dist2.count(i)) {
-                int maxDist = max(dist1[i], dist2[i]);
-                if (maxDist < minDist || (maxDist == minDist && i < ans)) {
-                    minDist = maxDist;
-                    ans = i;
-                }
+        int n=edges.size();
+        vector<bool>visited(n,false);
+        vector<int>v1(n,INT_MAX),v2(n,INT_MAX);
+        v1[node1]=0;
+        v2[node2]=0;
+        unordered_map<int,int>map;
+        for(int i=0;i<n;i++)
+        {
+            map[i]=edges[i];
+        }
+        solve(node1,map,visited,v1);
+        fill(visited.begin(),visited.end(),false);
+        solve(node2,map,visited,v2);
+        int ans=-1;
+        int dis=INT_MAX;
+        for(int i=0;i<n;i++)
+        {
+            if(v1[i]==INT_MAX || v2[i]==INT_MAX)
+            continue;
+            int t=max(v1[i],v2[i]);
+            if(dis>t)
+            {
+                dis=t;
+                ans=i;
             }
         }
-
         return ans;
     }
 };
