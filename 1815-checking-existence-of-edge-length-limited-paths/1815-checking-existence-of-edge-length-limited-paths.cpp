6e1c211bc7d6cf1d0077a8356829a0f9ad426433
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-07-26 18:58:08 +0530

    Time: 93 ms (33.19%), Space: 121.2 MB (40.06%) - LeetHub

--- 1815-checking-existence-of-edge-length-limited-paths/1815-checking-existence-of-edge-length-limited-paths.cpp
+++ 1815-checking-existence-of-edge-length-limited-paths/1815-checking-existence-of-edge-length-limited-paths.cpp
@@ -1,75 +1,80 @@
-class Solution {
-public:
-vector<int>parent;
-vector<int>rank;
-int find(int x)
-{
-    if(parent[x]==x)
-    return x;
-    return parent[x]=find(parent[x]);
-
-}
-void Union(int x,int y)
-{
-    int x_parent=find(x);
-    int y_parent=find(y);
-    if(x_parent==y_parent)
+class DSU{
+    vector<int>parent,rank;
+    public:
+    DSU(int n)
     {
-        return ;
-    }
-    if(rank[x_parent]>rank[y_parent])
-    {
-        parent[y_parent]=x_parent;
-    }
-    else if(rank[y_parent]>rank[x_parent])
-    {
-         parent[x_parent]=y_parent;
-    }
-    else
-    {
-         parent[y_parent]=x_parent;
-         rank[x_parent]++;
-    }
-}
-    vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
         parent.resize(n);
         rank.resize(n,0);
         for(int i=0;i<n;i++)
         {
             parent[i]=i;
+
         }
-        int i=0;
-        for (int i = 0; i < queries.size(); ++i) {
-            queries[i].push_back(i);  // Append the original index to each query
+    }
+        int find(int a)
+        {
+            if(parent[a]!=a)
+            parent[a]=find(parent[a]);
+            return parent[a];
         }
-        //sort on the basis of threshold
+        void Unite(int a ,int b)
+        {
+            int x=find(a);
+            int y=find(b);
+            if(x==y)
+            return ;
+            else if(rank[x]>rank[y])
+            {
+                parent[y]=x;
+
+            }
+            else if(rank[x]<rank[y])
+            {
+                parent[x]=y;
+            }
+            else
+            {
+                parent[y]=x;
+                rank[x]++;
+            }
+        }
+    
+};
+class Solution {
+public:
+
+    vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
+        DSU dsu(n);
         auto lambda=[&](vector<int>&v1,vector<int>&v2)
         {
             return v1[2]<v2[2];
         };
-        sort(queries.begin(),queries.end(),lambda);
-        sort(edgeList.begin(),edgeList.end(),lambda);
-        int j=0;
+        for(int i=0;i<queries.size();i++)
+        {
+            queries[i].push_back(i);
+        }
         vector<bool>ans(queries.size(),false);
+        sort(edgeList.begin(),edgeList.end(),lambda);
+        sort(queries.begin(),queries.end(),lambda);
+        int j=0;
         for(auto l:queries)
         {
             int a=l[0];
             int b=l[1];
             int c=l[2];
             int d=l[3];
-           for(int s=j;s<edgeList.size();s++)
-           {
-            if(edgeList[s][2]<c)
+            for(int i=j;i<edgeList.size();i++)
             {
-                Union(edgeList[s][0],edgeList[s][1]);
-                j++;
+                if(edgeList[i][2]<c)
+                {
+                    dsu.Unite(edgeList[i][0],edgeList[i][1]);
+                    j++;
+                }
+                else
+                break;
             }
-            else
-            break;
-           }
-           if(find(a)==find(b))
-            ans[d]=true;
-            
+            if(dsu.find(a)==dsu.find(b))
+            ans[d]=true;   
         }
         return ans;
     }
