b5db05de750d2fb023bf6b440d4c229e8bacab33
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-07-29 14:08:19 +0530

    Time: 1 ms (24.58%), Space: 9.4 MB (33.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
-vector<vector<int>>directions={{1,0},{0,1}};
-int dp[101][101];
-int  solve(int i,int j,int end_i,int end_j,vector<vector<bool>>&visited)
-{
-    if(i==end_i && j==end_j)
-    {
-       
-        return 1;
-    }
-    if(dp[i][j]!=-1)
-    return dp[i][j];
-    int ans=0;
-    visited[i][j]=true;
-    for(vector<int>dir:directions)
-    {
-        int new_i=dir[0]+i;
-        int new_j=dir[1]+j;
-        if(new_i>=0 && new_i<=visited.size()-1 && new_j>=0 && new_j<=visited[0].size()-1 && visited[new_i][new_j]==false)
-        {
-            if(solve(new_i,new_j,end_i,end_j,visited))
-            ans+=solve(new_i,new_j,end_i,end_j,visited);
-        }
-    }
-
-     visited[i][j]=false;
-     return dp[i][j]=ans;
-
-}
     int uniquePaths(int m, int n) {
-        vector<vector<bool>>visited(m,vector<bool>(n,false));
-        memset(dp,-1,sizeof(dp));
-         return solve(0,0,m-1,n-1,visited);
-       
+
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<n;i++)
+        {
+            dp[m-1][i]=1;
+        }
+         for(int i=0;i<m;i++)
+        {
+            dp[i][n-1]=1;
+        }
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j=n-2;j>=0;j--)
+            {
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
 
     }
 };
