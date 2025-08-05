1752c82cd01de28cffb9400a47140a70b29666e6
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-08-05 13:53:17 +0530

    Time: 184 ms (24.75%), Space: 13.8 MB (54.49%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
+int t[1001][1001];
+bool solve(string &s,int i,int j)
+{
+    if(i>=j)
+    {
+        return true;
+    }
+    if(t[i][j]!=-1)
+    return t[i][j];
+
+    if(s[i]==s[j])
+    return t[i][j]=solve(s,i+1,j-1);
+    
+        return t[i][j]=false;
+   
+}
     string longestPalindrome(string s) {
-        int n = s.size();
-        int maxLen = 0, start = 0;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j >= i; j--) {
-                int len = j - i + 1;
-
-                // Only check if current length is greater than maxLen
-                if (len <= maxLen) break;
-
-                // Check if substring s[i..j] is a palindrome
-                bool isPalindrome = true;
-                for (int l = i, r = j; l < r; l++, r--) {
-                    if (s[l] != s[r]) {
-                        isPalindrome = false;
-                        break;
+        int n=s.length();
+        int maxlen=INT_MIN;
+        int sp=0;
+        memset(t,-1,sizeof(t));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i;j<n;j++)
+            {
+                if(solve(s,i,j)==true)
+                {
+                    if(j-i+1>maxlen)
+                    {
+                        maxlen=j-i+1;
+                        sp=i;
                     }
-                }
-
-                if (isPalindrome) {
-                    maxLen = len;
-                    start = i;
-                    break; // No need to check smaller j for same i
                 }
             }
         }
-
-        return s.substr(start, maxLen);
+        return s.substr(sp,maxlen);
     }
 };
