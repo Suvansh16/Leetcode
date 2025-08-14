c580c7a69a0f8cf13c0ad96b2464cf607c48e14f
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-08-14 16:42:04 +0530

    Time: 0 ms (100%), Space: 36.1 MB (95.95%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.cpp
@@ -1,51 +1,39 @@
-/************************************************ C++ ************************************************/
-//Approach-1 (Using DSU)
 class Solution {
 public:
-    vector<int> parent;
-    int components;
-
-    int find(int x) {
-        if(parent[x] == x)
-            return x;
-        
-        return parent[x] = find(parent[x]);
-    }
-
-    bool Union(int par, int child) {
-        int child_ka_parent  = find(child);
-    
-        //child_ka_parent != child --> Child had already some other parent
-        if(child_ka_parent != child)
-            return false;
-        
-        int parent_ka_parent = find(par);
-        //parent_ka_parent == child_ka_parent ---> Parallel edge (It means, already they were connected by another edge)
-        if (parent_ka_parent == child_ka_parent) {
-            return false;
-        }
-        
-        parent[child] = par;
-        components--;
-        return true;
-    }
-    
+vector<int>parent;
+int components;
+int find(int x)
+{
+    if(parent[x]==x)
+    return x;
+    return parent[x]=find(parent[x]);
+}
+bool union_find(int par,int child)
+{
+    int child_par=find(child);
+    if(child_par!=child)
+    return false;
+    int par_par=find(par);
+    if(par_par==child_par)
+    return false;
+    parent[child]=par;
+    components--;
+    return true;
+}
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        components = n;
+        components=n;
         parent.resize(n);
-        for(int i = 0; i<n; i++) {
-            parent[i] = i;
+        for(int i=0;i<n;i++)
+        {
+            parent[i]=i;
         }
-
-        for (int i = 0; i < n; i++) {
-            if (leftChild[i] >= 0 && !Union(i, leftChild[i])) {
-                return false;
-            }
-            if (rightChild[i] >= 0 && !Union(i, rightChild[i])) {
-                return false;
-            }
+        for(int i=0;i<n;i++)
+        {
+            if(leftChild[i]>=0 && !union_find(i,leftChild[i]))
+            return false;
+            if(rightChild[i]>=0 && !union_find(i,rightChild[i]))
+            return false;
         }
-        
-        return components == 1;
+        return components==1;
     }
 };
