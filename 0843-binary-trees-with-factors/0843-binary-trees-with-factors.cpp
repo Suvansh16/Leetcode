928c6354e5b53a53c4c1ae21f1b039c4997897bc
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-05 13:51:21 +0530

    Time: 23 ms (55.13%), Space: 12.7 MB (92.97%) - LeetHub

--- 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
+++ 0843-binary-trees-with-factors/0843-binary-trees-with-factors.cpp
@@ -1,51 +1,28 @@
 class Solution {
 public:
-    int numFactoredBinaryTrees(vector<int>& v) {
-        sort(v.begin(), v.end());
-        unordered_map<int, long long> dp;
-        unordered_set<int> s(v.begin(), v.end());
-        long long mod = 1e9 + 7;
-        long long ans = 0;
-
-        for (int i = 0; i < v.size(); i++) {
-            dp[v[i]] = 1; // Each element can be a single-node tree
-            for (int j = 0; j < i; j++) {
-                if (v[i] % v[j] == 0) { // v[j] is a factor
-                    int x = v[i] / v[j];
-                    if (s.count(x)) { // Ensure the other factor is present in the array
-                        dp[v[i]] = (dp[v[i]] + (dp[v[j]] * dp[x])) ;
+int mod=1e9+7;
+    int numFactoredBinaryTrees(vector<int>& arr) {
+       sort(arr.begin(),arr.end());
+        unordered_map<int,long long >map;
+        for(int i:arr)
+        map[i]=1;
+        int ans=0;
+        for(int i=1;i<arr.size();i++)
+        {
+            for(int j=0;j<i;j++)
+            {
+                if(arr[i]%arr[j]==0)
+                {
+                    int sec=arr[i]/arr[j];
+                    if(map.find(sec)!=map.end())
+                    {
+                        map[arr[i]]=(map[arr[i]]%mod+(map[arr[j]]%mod*map[sec]%mod)%mod)%mod;
                     }
                 }
             }
-            ans = (ans + dp[v[i]]) % mod;
         }
+        for(auto i:map)
+        ans=(ans%mod+i.second%mod)%mod;
         return ans;
     }
 };
-
-
-// class Solution {
-// public:
-//     int numFactoredBinaryTrees(vector<int>& v) {
-//         unordered_set<int>s;
-//         sort(v.begin(),v.end());
-//         for (auto it:v){
-//             s.insert(it);
-//         }
-//         int op=0;
-//         unordered_map<int,int>m;
-//         int ans=0;
-//         for (int i =0;i<v.size();i++){
-//             for (int j=0;j<v.size();j++){
-//                 int x= v[i]/v[j];
-//                 int m=v[i]%v[j];
-//                 if (m.find(v[i])!=m.end()) ans+=m[v[i]];
-//                 if (m.find(v[j])!=m.end()) ans+=m[v[j]];
-//                 if (s.find(x)!=s.end() && m==0) ans++;
-//             }
-//             op+=ans;
-//             m[x]=ans;
-//         }
-//         return op+v.size();
-//     }
-// };
