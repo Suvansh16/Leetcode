bf1444ced757a4169e50381e993e4ede371a257a
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-07 18:18:25 +0530

    Time: 9 ms (81.08%), Space: 62.8 MB (51.44%) - LeetHub

--- 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/2472-build-a-matrix-with-conditions.cpp
@@ -1,64 +1,72 @@
 class Solution {
 public:
 
-bool solve(vector<bool>&visited,vector<bool>&inrecursion,vector<int>&ans,int val,unordered_map<int,vector<int>>&map)
-{
-    visited[val]=true;
-    inrecursion[val]=true;
-    for(auto i:map[val])
+    bool solve(int start, vector<bool>& visited, vector<bool>& inrecursion,
+               unordered_map<int, vector<int>>& graph, vector<int>& output) 
     {
-        if(!visited[i])
-        {
-            if(!solve(visited,inrecursion,ans,i,map))
-            return false;
-        }
-        else if(inrecursion[i])
-        return false;
+        if (inrecursion[start]) return false;   // cycle
+        if (visited[start]) return true;
 
-    }
-    ans.push_back(val);
-    inrecursion[val]=false;
-    return true;
-}
+        visited[start] = true;
+        inrecursion[start] = true;
 
-vector<int>topologicalsort( vector<vector<int>>& v,int k)
-{
-    unordered_map<int,vector<int>>map;
-    for(vector<int>&i:v)
-    {
-       map[i[0]].push_back(i[1]);
+        for (int i : graph[start]) {
+            if (!solve(i, visited, inrecursion, graph, output))
+                return false;
+        }
+
+        inrecursion[start] = false;
+        output.push_back(start);  // push AFTER recursion for topo order
+        return true;
     }
-    vector<int>ans;
-    vector<bool>visited(k+1,false);
-    vector<bool>inrecursion(k+1,false);
-    for(int i=1;i<=k;i++)
-    {
-        if(!visited[i])
-        {
-            if(!solve(visited,inrecursion,ans,i,map))
-            return {};
+
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& r, vector<vector<int>>& c) {
+        unordered_map<int, vector<int>> map1, map2;
+
+        // Fix edge direction: a -> b (a before b)
+        for (auto& i : r) map1[i[0]].push_back(i[1]);
+        for (auto& i : c) map2[i[0]].push_back(i[1]);
+
+        vector<bool> visited(k+1, false);
+        vector<int> seq1;
+
+        // Topo sort for rows
+        for (int i = 1; i <= k; i++) {
+            if (visited[i]) continue;
+            vector<bool> inrecursion(k+1, false);
+            if (!solve(i, visited, inrecursion, map1, seq1))
+                return {};
         }
-    }
-    reverse(ans.begin(),ans.end());
-    return ans;
-}
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        vector<int>rowcond=topologicalsort(rowConditions,k);
-        vector<int>colcond=topologicalsort(colConditions,k);
-        if(rowcond.empty() || colcond.empty())
-        return {};
-        unordered_map<int,int>rowpos;
-        unordered_map<int,int>colpos;
-        for(int i=0;i<k;i++)
-        {
-            rowpos[rowcond[i]]=i;
-            colpos[colcond[i]]=i;
+        reverse(seq1.begin(), seq1.end());
+
+        // Reset visited
+        fill(visited.begin(), visited.end(), false);
+        vector<int> seq2;
+
+        // Topo sort for cols
+        for (int i = 1; i <= k; i++) {
+            if (visited[i]) continue;
+            vector<bool> inrecursion(k+1, false);
+            if (!solve(i, visited, inrecursion, map2, seq2))
+                return {};
         }
-        vector<vector<int>>result(k,vector<int>(k,0));
-        for(int i=1;i<=k;i++)
-        {
-            result[rowpos[i]][colpos[i]]=i;
+        reverse(seq2.begin(), seq2.end());
+
+        // Build result
+        vector<vector<int>> result(k, vector<int>(k, 0));
+        unordered_map<int, int> rowPos, colPos;
+
+        for (int i = 0; i < k; i++) {
+            rowPos[seq1[i]] = i;
+            colPos[seq2[i]] = i;
         }
+
+        for (int num = 1; num <= k; num++) {
+            int r = rowPos[num];
+            int cpos = colPos[num];
+            result[r][cpos] = num;
+        }
+
         return result;
     }
 };
