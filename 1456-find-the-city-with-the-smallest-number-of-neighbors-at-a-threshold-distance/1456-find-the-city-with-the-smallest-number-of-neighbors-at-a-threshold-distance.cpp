77f1348855eb7f9e3ca8e361a83076cecaeb2789
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-08 21:14:37 +0530

    Time: 79 ms (5.81%), Space: 41.9 MB (5.02%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,40 +1,58 @@
 class Solution {
 public:
-    int findTheCity(int n, vector<vector<int>>& edges, int dt) {
-        vector<vector<int>>dist(n,vector<int>(n,1e9));
-        for(auto i:edges)
+void dijkstra(int i, unordered_map<int,vector<pair<int,int>>>map, vector<vector<int>>&adjmatrix)
+{
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    pq.push({0,i});
+    while(!pq.empty())
+    {
+        int dist=pq.top().first;
+        int node=pq.top().second;
+        pq.pop();
+        for(auto &it:map[node])
         {
-            dist[i[0]][i[1]]=i[2];
-            dist[i[1]][i[0]]=i[2];
-        }
-        for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
+            int edge=it.first;
+            int cost=it.second;
+            if(adjmatrix[i][edge]>dist+cost)
             {
-                if(dist[i][k]+dist[k][j]<dist[i][j])
-                {
-                    dist[i][j]=dist[i][k]+dist[k][j];
-                }
+                adjmatrix[i][edge]=dist+cost;
+                pq.push({dist+cost,edge});
             }
         }
-
+    }
+}
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        unordered_map<int,vector<pair<int,int>>>map;
+        for(int i=0;i<edges.size();i++)
+        {
+            map[edges[i][0]].push_back({edges[i][1],edges[i][2]});
+            map[edges[i][1]].push_back({edges[i][0],edges[i][2]});
         }
-        int mini=INT_MAX,idx=-1;
+        vector<vector<int>>adjmatrix(n,vector<int>(n,INT_MAX));
+        for(int i=0;i<n;i++)
+        {
+            dijkstra(i,map,adjmatrix);
+        }
+        int ans=-1;
+        int mincount=INT_MAX;
         for(int i=0;i<n;i++)
         {
             int count=0;
             for(int j=0;j<n;j++)
             {
-                if(i!=j && dist[i][j]<=dt)
-                count++;
+                if(i==j)
+                continue;
+                if(adjmatrix[i][j]<=distanceThreshold)
+                {
+                    count++;
+                }
             }
-            if(count<=mini)
+            if(count<=mincount)
             {
-                mini=count;
-                idx=i;
+                mincount=count;
+                ans=i;
             }
         }
-        return idx;
+        return ans;
     }
 };
