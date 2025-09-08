f580223e636abade89b13de5f79a1ac7fde84b00
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-08 21:53:40 +0530

    Time: 22 ms (47.72%), Space: 26.1 MB (10.14%) - LeetHub

--- 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
+++ 0984-most-stones-removed-with-same-row-or-column/0984-most-stones-removed-with-same-row-or-column.cpp
@@ -1,52 +1,49 @@
-class DSU{
-
-    public:
-    unordered_map<int,int>parent;
-    int find(int x)
-    {
-        if(!parent.count(x))
-        parent[x]=x;
-        if(parent[x]!=x)
-        parent[x]=find(parent[x]);
-        return parent[x];
-    }
-    void unite(int a,int b)
-    {
-        int px=find(a);
-        int py=find(b);
-        if(px!=py)
-        parent[px]=py;
-    }
-};
 class Solution {
 public:
+    int solve(int start, unordered_set<int>& visited, unordered_map<int, vector<int>>& graph) {
+        int count = 1;
+        visited.insert(start);
+
+        for (int nei : graph[start]) {
+            if (visited.find(nei) == visited.end()) {
+                count += solve(nei, visited, graph);
+            }
+        }
+        return count;
+    }
+
     int removeStones(vector<vector<int>>& stones) {
-        DSU dsu;
-        unordered_map<int,vector<int>>map;
-        for(int i=0;i<stones.size();i++)
-        {
-            for(int j=i+1;j<stones.size();j++)
-            {
-                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1])
-                {map[i].push_back(j);
-                map[j].push_back(i);}
+        unordered_map<int,int> row;
+        unordered_map<int,int> col;
+        unordered_map<int,vector<int>> graph;
 
+        for (int i = 0; i < stones.size(); i++) {
+            int a = stones[i][0], b = stones[i][1];
+
+            if (row.find(a) != row.end()) {
+                graph[i].push_back(row[a]);
+                graph[row[a]].push_back(i);
+            } else {
+                row[a] = i;
+            }
+
+            if (col.find(b) != col.end()) {
+                graph[i].push_back(col[b]);
+                graph[col[b]].push_back(i);
+            } else {
+                col[b] = i;
             }
         }
-        for(auto i:map)
-        {
-            int a=i.first;
-            for(int j:i.second)
-            {
-                dsu.unite(a,j);
+
+        unordered_set<int> visited;
+        int ans = 0;
+
+        for (int i = 0; i < stones.size(); i++) {
+            if (visited.find(i) == visited.end()) {
+                int l = solve(i, visited, graph);
+                ans += (l - 1);  // ✅ add instead of max
             }
         }
-        unordered_set<int> uniqueRoots;
-        for (int i = 0; i < stones.size(); ++i) {
-            uniqueRoots.insert(dsu.find(i));
-        }
-
-        // Max removable stones = total - number of components
-        return stones.size() - uniqueRoots.size();
+        return ans;
     }
 };
