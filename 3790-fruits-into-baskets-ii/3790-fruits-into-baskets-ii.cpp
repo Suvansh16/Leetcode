9cca2ec5688c267eb816c275beded11c02f11ca8
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-13 18:13:54 +0530

    Time: 4 ms (31.35%), Space: 50.3 MB (6.51%) - LeetHub

--- 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -1,25 +1,50 @@
+
 class Solution {
 public:
-    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        vector<bool>t(baskets.size(),false);
-        int count=0;
-        for(int i=0;i<fruits.size();i++)
-        {
-            int a=fruits[i];
-            bool m=false;
-            for(int j=0;j<baskets.size();j++)
-            {
-                if(!t[j] && baskets[j]>=a)
-                {
-                    t[j]=true;
-                    m=true;
-                    break;
+void build(int i,int l,int r,vector<int>&seg,vector<int>&heights)
+{
+    if(l==r)
+    {
+        seg[i]=heights[l];
+        return ;
+    }
+    int mid=(l+r)/2;
+    build(2*i+1,l,mid,seg,heights);
+    build(2*i+2,mid+1,r,seg,heights);
+    seg[i]=max(seg[2*i+1],seg[2*i+2]);
+}
+bool query(int i,int l,int r,vector<int>&seg,int val)
+{
+    if(seg[i]<val)
+    return false;
+    if(l==r)
+    {
+        seg[i]=-1;
+        return true;
 
-                }
-            }
-            if(m==false)
-            count++;
+    }
+    int mid=(l+r)/2;
+    bool placed=false;
+    if(seg[2*i+1]>=val)
+    {
+        placed=query(2*i+1,l,mid,seg,val);   
+    }
+    else
+    {
+        placed=query(2*i+2,mid+1,r,seg,val);
+    }
+    seg[i]=max(seg[2*i+1],seg[2*i+2]);
+    return placed;
+}
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        vector<int>seg(4*baskets.size(),-1);
+        build(0,0,baskets.size()-1,seg,baskets);
+        int unplaced=0;
+        for(int i:fruits)
+        {
+            if(!query(0,0,baskets.size()-1,seg,i))
+            unplaced++;
         }
-        return count;
+        return unplaced;
     }
 };
