d36ac87c46800ed2fc725e96c752622574834d6b
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-15 08:44:17 +0530

    Time: 84 ms (63.26%), Space: 204.7 MB (5.18%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-void build(int i,int l,int r,vector<int>&seg,vector<int>&heights)
+int n;
+
+void build(int i,int l,int r,vector<int>&seg,vector<int>&baskets)
 {
     if(l==r)
     {
-        seg[i]=heights[l];
+        seg[i]=baskets[l];
         return ;
     }
     int mid=(l+r)/2;
-    build(2*i+1,l,mid,seg,heights);
-    build(2*i+2,mid+1,r,seg,heights);
-    seg[i]=max(seg[2*i+1],seg[2*i+2]);
+    build(2*i+1,l,mid,seg,baskets);
+    build(2*i+2,mid+1,r,seg,baskets);
+    seg[i]=max(seg[2*i+2],seg[2*i+1]);
+    return ;
 }
-bool query(int i,int l,int r,vector<int>&seg,int val)
+bool query(int i,int val,int l,int r,vector<int>&seg)
 {
     if(seg[i]<val)
     return false;
@@ -20,28 +23,30 @@ bool query(int i,int l,int r,vector<int>&seg,int val)
     {
         seg[i]=-1;
         return true;
-
     }
-    int mid=(l+r)/2;
     bool placed=false;
+    int mid=(l+r)/2;
     if(seg[2*i+1]>=val)
     {
-        placed=query(2*i+1,l,mid,seg,val);   
+        
+        placed=query(2*i+1,val,l,mid,seg);
     }
     else
     {
-        placed=query(2*i+2,mid+1,r,seg,val);
+        placed=query(2*i+2,val,mid+1,r,seg);
     }
     seg[i]=max(seg[2*i+1],seg[2*i+2]);
     return placed;
 }
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        vector<int>seg(4*baskets.size(),-1);
-        build(0,0,baskets.size()-1,seg,baskets);
+        n=baskets.size();
+        vector<int>seg(4*n);
+        build(0,0,n-1,seg,baskets);
         int unplaced=0;
         for(int i:fruits)
         {
-            if(!query(0,0,baskets.size()-1,seg,i))
+            
+            if(!query(0,i,0,n-1,seg))
             unplaced++;
         }
         return unplaced;
