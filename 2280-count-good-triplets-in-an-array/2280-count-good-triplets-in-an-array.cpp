8578853d061a9a743fc9711f866530ce6e32b4c5
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-09-15 21:28:01 +0530

    Time: 154 ms (44.04%), Space: 120.3 MB (34.77%) - LeetHub

--- 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.cpp
+++ 2280-count-good-triplets-in-an-array/2280-count-good-triplets-in-an-array.cpp
@@ -1,42 +1,55 @@
 class Solution {
 public:
-    using ll = long long;
-    vector<ll> bit1, bit2;
-    int n;
-
-    void update(vector<ll>& bit, int i, ll val) {
-        for (++i; i <= n; i += (i & -i))
-            bit[i] += val;
+void update(int i,int l,int r,int update_idx,vector<long long>&seg)
+{
+    if(l==r)
+    {
+        seg[i]=1;
+        return;
     }
-
-    ll query(const vector<ll>& bit, int i) {
-        ll res = 0;
-        for (++i; i > 0; i -= (i & -i))
-            res += bit[i];
-        return res;
+    int mid=(l+r)/2;
+    if(update_idx<=mid)
+    {
+        update(2*i+1,l,mid,update_idx,seg);
     }
-
+    else
+    {
+        update(2*i+2,mid+1,r,update_idx,seg);
+    }
+    seg[i]=seg[2*i+1]+seg[2*i+2];
+}
+long long query(int start,int end,int i,int l,int r,vector<long long>&seg)
+{
+    if(r<start || l>end)
+    return 0;
+    if(l>=start && r<=end)
+    return seg[i];
+    int mid=(l+r)/2;
+    long long left=query(start,end,2*i+1,l,mid,seg);
+    long long right=query(start,end,2*i+2,mid+1,r,seg);
+    return left+right;
+}
     long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size();
-        vector<int> pos(n);
-        for (int i = 0; i < n; ++i)
-            pos[nums2[i]] = i;
-        for (int i = 0; i < n; ++i)
-            nums1[i] = pos[nums1[i]];
+        unordered_map<int,int>map;
+       for(int i=0;i<nums2.size();i++)
+       {
+        map[nums2[i]]=i;
+       }
+       int n=nums1.size();
+       vector<long long >seg(4*n);
+       long long result=0;
+       update(0,0,n-1,map[nums1[0]],seg);
+       for(int i=1;i<n;i++)
+       {
+        int idx=map[nums1[i]];
+        long long left_common=query(0,idx,0,0,n-1,seg);
+        long long left_not_common=i-left_common;
+        long long elements_after_idx=(n-1)-idx;
+        long long right_common=elements_after_idx-left_not_common;
+        result+=left_common*right_common;
+        update(0,0,n-1,idx,seg);
+       }
+       return result;
 
-        bit1.assign(n + 2, 0);
-        bit2.assign(n + 2, 0);
-        ll ans = 0;
-
-        for (int i = n - 1; i >= 0; --i) {
-            int x = nums1[i];
-            ll val = query(bit1, n - 1) - query(bit1, x);
-            ll trip = query(bit2, n - 1) - query(bit2, x);
-            ans += trip;
-            update(bit2, x, val);
-            update(bit1, x, 1);
-        }
-
-        return ans;
     }
 };
