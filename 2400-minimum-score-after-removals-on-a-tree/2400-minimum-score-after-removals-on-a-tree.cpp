42c0a74c5c2ee8c127aa68721335cbd601ae61ef
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-10-01 15:15:34 +0530

    Time: 123 ms (68.32%), Space: 19.3 MB (42.63%) - LeetHub

---
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -0,0 +1,74 @@
+class Solution {
+public:
+int getscore(int a,int b,int c)
+{
+    int maxi=max({a,b,c});
+    int mini=min({a,b,c});
+    return maxi-mini;
+}
+bool isancestor(int a,int b,vector<int>&intime,vector<int>&outtime){
+    return intime[b]>=intime[a] && outtime[a]>=outtime[b];
+}
+void dfs(int node,int parent,vector<int>&subtree,vector<int>&intime,vector<int>&outtime,int &timer,vector<int>&nums,unordered_map<int,vector<int>>&map)
+{
+    subtree[node]=nums[node];
+    intime[node]=timer;
+    timer++;
+    for(int &nei:map[node])
+    {
+        if(nei!=parent)
+        {
+            dfs(nei,node,subtree,intime,outtime,timer,nums,map);
+            subtree[node]^=subtree[nei];
+        }
+    }
+    outtime[node]=timer;
+    timer++;
+}
+    int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
+        int timer=0,n=nums.size();
+        unordered_map<int,vector<int>>map;
+        for(auto i:edges)
+        {
+            map[i[0]].push_back(i[1]);
+            map[i[1]].push_back(i[0]);
+        }
+
+        vector<int>subtree(n,0);
+        vector<int>intime(n,0);
+        vector<int>outtime(n,0);
+        dfs(0,-1,subtree,intime,outtime,timer,nums,map);
+        int result=INT_MAX;
+        for(int u=1;u<n;u++)
+        {
+            for(int v=u+1;v<n;v++)
+            {
+                int xor1,xor2,xor3;
+                if(isancestor(u,v,intime,outtime))
+                {
+                     xor1=subtree[u];
+                    xor2=subtree[v];
+                    xor1=xor1^xor2;
+                    xor3=subtree[0]^xor1^xor2;
+                }
+                else if(isancestor(v,u,intime,outtime))
+                {
+                     xor1=subtree[v];
+                    xor2=subtree[u];
+                    xor1=xor1^xor2;
+                    xor3=subtree[0]^xor1^xor2;
+                }
+                else
+                {
+                    xor1=subtree[u];
+                    xor2=subtree[v];
+                    xor3=subtree[0]^xor1^xor2;
+
+                }
+                result=min(result,getscore(xor1,xor2,xor3));
+            }
+
+        }
+        return result;
+    }
+};
