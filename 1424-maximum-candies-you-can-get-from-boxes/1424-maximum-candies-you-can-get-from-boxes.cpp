fa75e6a942c75d7cb4d4463432f891d25e26c466
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-10-02 22:29:13 +0530

    Time: 12 ms (31.42%), Space: 47 MB (29.22%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
-int solve(int start,vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, unordered_set<int>&visited,unordered_set<int>&foundbox)
-{
-    if(visited.count(start))
-    return 0;
-    if(status[start]==0)
-    {
-        foundbox.insert(start);
-        return 0;
-
-    }
-    visited.insert(start);
-    int ans=candies[start];
-    for(int i:containedBoxes[start])
-    {
-        ans+=solve(i,status,candies,keys,containedBoxes,visited,foundbox);
-    }
-    for(int i:keys[start])
-    {
-        status[i]=1;
-        if(foundbox.count(i))
-        {
-            ans+=solve(i,status,candies,keys,containedBoxes,visited,foundbox);
-        }
-    }
-    return ans;
-}
     int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
-        unordered_set<int>visited;
-        unordered_set<int>foundbox;
+        queue<int>q;
+        unordered_set<int>visited,foundbox;
         int ans=0;
         for(int i:initialBoxes)
         {
-            if(!visited.count(i))
-            ans+=solve(i,status,candies,keys,containedBoxes,visited,foundbox);
+            if(status[i] && !visited.count(i))
+            {
+                ans+=candies[i];
+                visited.insert(i);
+                q.push(i);
+            }
+           
+                foundbox.insert(i);
+         
+        }
+        while(!q.empty())
+        {
+            int a=q.front();
+            q.pop();
+            for(int i:containedBoxes[a])
+            {
+                if(status[i]==1 && !visited.count(i))
+                {
+                     ans+=candies[i];
+                    visited.insert(i);
+                    q.push(i);
+                }
+               
+                    foundbox.insert(i);
+                
+            }
+            for(int i:keys[a])
+            {
+                status[i]=1;
+                if(foundbox.count(i)  && !visited.count(i))
+                {
+                    ans+=candies[i];
+                    visited.insert(i);
+                    q.push(i);
+                }
+            }
+
         }
         return ans;
     }
