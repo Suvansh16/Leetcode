a53f23db2af1f1c82879fad8819bb7e18e1d1d8d
Suvansh Arora <152032051+Suvansh16@users.noreply.github.com>
2025-11-06 09:27:55 +0530

    Time: 0 ms (100%), Space: 27.6 MB (5.84%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        vector<char>ans;
+        for(int i=s.size()-1;i>=0;i--)
+        ans.push_back(s[i]);
+       s=ans;
+    }
+};
